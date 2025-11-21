c3b2da669f5e64f8a98d47c96b8bf321f4f679be
rishita-codes <rishitabijalwan7@gmail.com>
2025-11-21 20:06:53 +0530

    Update crawler.cpp

--- crawler.cpp
+++ crawler.cpp
@@ -1,60 +1,105 @@
-#pragma once
-#include <iostream>
-#include <fstream>
-#include <sstream>
-#include <string>
-#include <cctype>
-#include "indexer.cpp"  
+#include<iostream>
+#include<fstream>
+#include<vector>
+#include<filesystem>
+#include<memory>
+#include<sstream>
+#include<string>
+#include<algorithm>
+#include<cctype>
+#include<regex>
 
-class HTMLParser {
+class Crawler{
 public:
-    static std::string stripHTML(const std::string &html) {
-        std::string text;
-        bool inTag = false;
-        for (char c : html) {
-            if (c == '<') inTag = true;
-            else if (c == '>') inTag = false;
-            else if (!inTag) text += c;
-        }
-        return text;
+    struct FileInfo{
+        std::string fname;
+        int total_terms{0};
+        double score;
+    };
+    struct TokenNode{
+        std::string str;
+        std::unique_ptr<TokenNode> next;
+    };
+
+    static void insert_front(std::unique_ptr<TokenNode>& head ,std::string str){
+        auto k=std::make_unique<TokenNode>() ;
+        if(head==nullptr)
+            k->next=nullptr;
+        else
+            k->next=std::move(head);
+        k->str=str;
+        head=std::move(k);
     }
 
-    static std::string cleanWord(const std::string &word) {
-        std::string result;
-        for (char c : word) {
-            if (std::isalnum(static_cast<unsigned char>(c))) {
-                result += c;
-            }
+    static void display(std::unique_ptr<TokenNode>& head){
+        auto p= head.get();
+        while(p!=NULL){
+            std::cout<< p->str << " ";
+            p=(p->next).get();
         }
-        return result;
     }
 
-    static node* parseFile(const std::string &filename) {
+    static std::vector<FileInfo> fetch_file_names(std::string dir_path="/home/pop/Desktop/R_folder/new_cpp_proj/database01"){
+        std::vector<FileInfo> files;
+        FileInfo f;
+        for(const auto& entry: std::filesystem::directory_iterator(dir_path)){
+            f.fname= dir_path+"/"+entry.path().filename().string();
+            files.push_back(f);
+        }
+        return files;
+    }
+
+    static void clean_text(std::string &content){
+        std::transform( content.begin(), content.end(), content.begin(), ::tolower);
+        std::regex script_regex("<script\\b[^>]*>[\\s\\S]*?</script>");
+        content= std::regex_replace(content, script_regex, "");
+        std::regex style_regex("<style\\b[^>]*>[\\s\\S]*?</style>", std::regex_constants::icase);
+        content= std::regex_replace(content, style_regex, "");
+        std::regex tag_regex("<[^>]*>");
+        content= std::regex_replace(content, tag_regex, " "); 
+        std::regex special_char_regex("[^a-zA-Z0-9\\s]+");
+        content = std::regex_replace(content, special_char_regex, "");
+        std::regex space_regex("[\\s]+");
+        content= std::regex_replace(content, space_regex, " "); 
+    }
+
+    static void tokenize(Crawler::FileInfo &cur_file, std::unique_ptr<TokenNode>& token_list){
+        std::string filename=cur_file.fname;
         std::ifstream file(filename);
-        if (!file.is_open()) {
-            std::cerr << "Cannot open file: " << filename << "\n";
-            return nullptr;
+        if(!file.is_open())
+        {
+            std::cerr << "Error opening file " << filename << std::endl;
+            exit(1);
         }
-
         std::stringstream buffer;
         buffer << file.rdbuf();
-        std::string htmlContent = buffer.str();
-        file.close();
-
-        std::string cleanText = stripHTML(htmlContent);
-
-        node* head = nullptr;
+        std::string content= buffer.str();
+        clean_text(content);
+        std::stringstream con_stream(content);
         std::string word;
-        std::stringstream ss(cleanText);
-
-        while (ss >> word) {
-            word = cleanWord(word);
-            if (!word.empty())
-                linked_list::insert_end(&head, word);
+        std::vector<std::string> stop_words= {
+        "a", "an", "the", "and", "but", "or", "because", 
+        "as", "if", "of", "at", "by", "for", "with", 
+        "is", "are", "was", "were", "be", "been", "to", 
+        "in", "on", "from", "it", "its", "me", "my", 
+        "you", "your", "i", "he", "she", "we", "they",
+        "this", "that", "what", "where", "how", "am" };
+        int f;
+        while(con_stream >> word){
+            f=0;
+            for(std::string sword: stop_words)
+            {
+                if(word==sword)
+                {
+                    f=1;
+                    break;
+                }
+            }
+            if(f==0)
+            {
+                cur_file.total_terms++;
+                insert_front(token_list, word);
+            }
         }
-
-        token::tokenization(&head);
-
-        return head;
-    }
+    }    
 };
